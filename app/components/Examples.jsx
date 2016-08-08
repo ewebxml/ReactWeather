7f71182b7ec9e2722b3371fd9bc056c482332a12
ewebxml <ewebxml@gmail.com>
2016-08-07 23:25:54 -0400

    Add grid system and style

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,11 +1,20 @@
 var React = require('react');
-
+var {Link} = require('react-router');
 // Refactored using stateless React component
-var Examples = (propers) =>{
+var Examples = (props) =>{
     return (
         <div>
-        <h3>Examples </h3>
-            <p>Welcome to the examples page.</p>
+        <h1 className="text-center">Examples </h1>
+            <p>Here a few example locations to try out.</p>
+            <ol>
+                <li>
+                    <Link to='/?location=Auburn Hills'>Auburn Hills, MI   </Link>
+
+                </li>
+                <li>
+                    <Link to='/?location=Rio'>Rio, Brazil</Link>
+                </li>
+            </ol>
         </div>
     )
 };
