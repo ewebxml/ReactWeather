2317ca409380b38ee55b72a2d76331d83c4aef44
ewebxml <ewebxml@gmail.com>
2016-08-12 05:56:16 -0400

    Add error modal

--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -1,20 +1,21 @@
 var React = require('react');
 
 var WeatherForm = React.createClass({
-    onFormSubmit: function(e){
+    onFormSubmit: function (e) {
         e.preventDefault();
+
         var location = this.refs.location.value;
-        if(location.length > 0 ){
+
+        if (location.length > 0) {
             this.refs.location.value = '';
             this.props.onSearch(location);
         }
     },
-
     render: function () {
         return (
             <div>
                 <form onSubmit={this.onFormSubmit}>
-                    <input type="text" ref="location" />
+                    <input type="text" ref="location"/>
                     <button className="button expanded hollow">Get Weather</button>
                 </form>
             </div>
